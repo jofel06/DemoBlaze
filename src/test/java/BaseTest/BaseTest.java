2d27eb5aa1069cfb9d08669bbfcbf85347e2a537
Christian Nathorst <jofel.nathorst@gmail.com>
2025-01-30 16:00:18 +0800

    Updates, added Parallel test

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -1,5 +1,7 @@
 package BaseTest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -8,24 +10,28 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import utils.ConfigReader;
 
-
 public class BaseTest {
     protected WebDriver driver;
     public static String baseURl;
 
+    public static final Logger logger = LogManager.getLogger(BaseTest.class);
+
     @BeforeMethod
     @Parameters("browser")
     public void setUp(String browser){
-        // this loads the properties (base URL)
-        baseURl = ConfigReader.getBaseUrl();
+
+        baseURl = ConfigReader.getBaseUrl(); //this loads the properties (base URL)
 
         if (browser.equalsIgnoreCase("chrome")){
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\Christian\\IdeaProjects\\DemoBlaze\\src\\test\\resources\\Drivers\\chromedriver.exe");
             driver = new ChromeDriver();
+            logger.info("Starting test using Chrome browser");
+
         }
         else if (browser.equalsIgnoreCase("edge")){
             System.setProperty("webdriver.edge.driver", "C:\\Users\\Christian\\IdeaProjects\\DemoBlaze\\src\\test\\resources\\Drivers\\msedgedriver.exe");
             driver = new EdgeDriver();
+            logger.info("Starting Test using Edge browser");
         }
         driver.manage().window().maximize();
     }
@@ -34,6 +40,7 @@ public class BaseTest {
     public void tearDown(){
         if (driver != null){
             driver.quit();
+            logger.info("Browser closed");
         }
     }
 }
--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -14,21 +14,31 @@ import java.time.Duration;
 public class LoginTest extends BaseTest {
 
     @Test(dataProvider = "loginData", dataProviderClass = LoginDataUtil.class)
-    public void test_Login(String username, String password) throws InterruptedException{
+    public void test_Login(String username, String password){
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get(baseURl);
 
-        WebElement login_link = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='navbarExample']//a[@id='login2']")));
-        login_link.click();
+        try {
+            logger.info("Opening the website");
+            driver.get(baseURl);
+            logger.info("Navigating to the Login page");
+            WebElement login_link = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='navbarExample']//a[@id='login2']")));
+            login_link.click();
 
-        WebElement username_field = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginusername")));
-        WebElement password_field = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginpassword")));
-        username_field.sendKeys(username);
-        password_field.sendKeys(password);
 
-        WebElement login_button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='modal-footer']//button[@onclick='logIn()']")));
-        login_button.click();
+            WebElement username_field = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginusername")));
+            WebElement password_field = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginpassword")));
+            logger.info("Entering username");
+            username_field.sendKeys(username);
+            logger.info("Entering password");
+            password_field.sendKeys(password);
 
-        Thread.sleep(3000);
+            logger.info("Logging in");
+            WebElement login_button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='modal-footer']//button[@onclick='logIn()']")));
+            login_button.click();
+        }
+        catch (Exception e){
+            logger.error("Login failed due to an error: ", e);
+        }
     }
 }
