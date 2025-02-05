4695d05fe3af5cb45179e3344bf3668c3b4c0884
Christian Nathorst <jofel.nathorst@gmail.com>
2025-02-05 16:55:18 +0800

    Added Page Object Model

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -1,12 +1,11 @@
 package BaseTest;
 
+import Page_Objects.LoginPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.ThreadContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.slf4j.MDC;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -14,14 +13,13 @@ import utils.ConfigReader;
 
 public class BaseTest {
     protected WebDriver driver;
-    public static String baseURl;
+    protected static String baseURl;
     public static final Logger logger = LogManager.getLogger(BaseTest.class);
+    protected LoginPage loginPage;
 
     @BeforeMethod
     @Parameters("browser")
     public void setUp(String browser){
-        ThreadContext.put("browser", browser); // this sets the browser name in Log4j2 (Thread Context)
-        MDC.put("className", this.getClass().getSimpleName());  // Set test class name in MDC
 
         baseURl = ConfigReader.getBaseUrl(); //this loads the properties (base URL)
         String driverPath = ConfigReader.getDriverPath(browser); // this gets the correct driver path from config.properties
@@ -37,6 +35,12 @@ public class BaseTest {
             logger.info("Starting Test using Edge browser");
             }
         driver.manage().window().maximize();
+        loginPage = new LoginPage(driver);
+
+        // Add the step to navigate to the base URL after initializing the driver
+        logger.info("Navigating to base URL: " + baseURl);
+        driver.get(baseURl);  // This will open the URL in the browser
+
     }
 
     @AfterMethod
@@ -44,7 +48,6 @@ public class BaseTest {
         if (driver != null){
             driver.quit();
             logger.info("Browser closed");
-            MDC.remove("className");  // Remove the class name from MDC after the test
 
         }
     }
---
+++ src/test/java/Page_Objects/LoginPage.java
@@ -0,0 +1,60 @@
+package Page_Objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(id = "login2")
+    private WebElement loginLink;
+
+    @FindBy(id = "loginusername")
+    private WebElement usernameField;
+
+    @FindBy(id = "loginpassword")
+    private WebElement passwordField;
+
+    @FindBy(css = "button[onclick='logIn()']")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "(//div[@class='modal-footer']//button[text()='Close'])[2]")
+    private WebElement loginCloseButton;
+
+    @FindBy(xpath = "(//div[@class='modal-header']/button[@class='close'])[2]")
+    private WebElement loginCloseIcon;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this); //this initializes the WebElement fields in this class by using the WebDriver to locate them on the page, the @FindBy.
+    }
+
+    public void clickLoginLink(){
+        click(loginLink);
+    }
+
+    public void enterUsername(String username){
+        type(usernameField, username);
+    }
+
+    public void enterPassword(String password){
+        type(passwordField, password);
+    }
+
+    public void clickLoginButton(){
+        click(loginButton);
+    }
+
+    public void clickLoginCloseButton(){
+        click(loginCloseButton);
+    }
+
+    public void clickLoginCloseIcon(){
+        click(loginCloseIcon);
+    }
+
+
+}
+
+
